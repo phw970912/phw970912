adba4b9829c1297177e71b7c277a0128242f88cf
phw970912 <phw970912.naver.com>
2019-10-29 11:18:14 +0900

    commit2

--- main3.js
+++ main3.js
@@ -182,11 +182,30 @@ http.createServer(function (request, response) {
 }).listen(52274, function() {
 	console.log('Server Running at http://127.0.0.1:52274');
 });*/
-
+/*
 var http = require('http');
 http.createServer(function (request, response) {
 	response.writeHead(200, { 'Content-Type' : 'text/html'});
 	response.end('<h1>Test - File - 1</h1>');
 }).listen(52273, function() {
+	console.log('Server Running at http://127.0.0.1:52273');
+});*/
+
+require('http').createServer(function (request, response) {
+	if (request.url == '/') {
+		response.write('<!DOCTYPE html>');
+		response.write('<html>');
+		response.write('<head>');
+		response.write('<title>forever</title>');
+		response.write('</head>');
+		response.write('<body>');
+		response.write('<h1>Forever</h1>');
+		response.write('</body>');
+		response.write('</html>');
+		response.end();
+	} else {
+		error.error.error();
+	}
+}).listen(52273, function () {
 	console.log('Server Running at http://127.0.0.1:52273');
 });
